e2ebd93021feb4a16b1bd6a108d949b348a38517
Pattu Raja <146655306+patturaja501@users.noreply.github.com>
2025-06-10 12:35:24 +0530

    Add files via upload

---
+++ Sprint 2/task 1/jstask1.js
@@ -0,0 +1,94 @@
+// task 1
+console.log("hello world");
+
+//  task 2
+let name="Pattu raja";
+console.log(name);
+
+// task 3
+
+let name="Pattu raja";
+let age="21";
+console.log("name is:",name);
+console.log("age is:",age);
+
+// task 4
+let name= "Pattu raja";
+let age=21;
+let a=20;
+console.log("Name is:",name);
+console.log("Age is:",age);
+if(a>18){
+    console.log("This is a big number");
+}
+else{
+    console.log("This is a small number");
+}
+// task 5
+    let name="Pattu Raja";
+    let age=21;
+    console.log("Name is "+name,"Age is:"+age);
+
+    // task 6
+    var name="pattu raja";
+    var age=21;
+    console.log("Name is;",name);
+    console.log("Age is;",age)
+    //  task 7
+    var a=10;
+    var b=15;
+    console.log("Addition;",a+b);
+    console.log("subtraction;",a-b);
+    console.log("multiplication;",a*b);
+    console.log("division;",a%b);
+    // task 8
+    var number=["one","two","three","four","five"];
+    for (let i = 0; i < number.length; i++) {
+        
+         console.log(number[i]);
+    }
+//    task 9
+    let n=5
+    for (let i = 0; i <=n; i++) { 
+        let pattern ='';
+    for (let j = 0; j <=i; j++){
+         pattern +='*';
+    }
+    
+     console.log(pattern);
+}
+// task 10
+var name="Pattu Raja";
+var age=21;
+var isstudent=true;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isstudent);
+// task 11
+var name="Pattu Raja";
+var age=21;
+var isstudent=true;
+console.log(name);
+console.log(age);
+console.log(isstudent);
+name="raja";
+age=age+1;
+isstudent=!isstudent;
+console.log(name);
+console.log(age);
+console.log(isstudent);
+    // task 12
+var name="Pattu Raja";
+var age=21;
+var Dob= "09-09-2003";
+var height= 171.3;
+var weight= 55.5;
+var degree="BE(Computer Sceince and Engneering )";
+var gender="male";
+console.log("Name is;",name);
+console.log("Age is;",age);
+console.log("Date of birth;",Dob);  
+console.log("Height;",height);
+console.log("Weight;",weight);
+console.log("Degree;",degree);
+console.log("Gender;",gender);
