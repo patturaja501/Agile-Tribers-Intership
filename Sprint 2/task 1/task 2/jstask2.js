4e42dcaa3c85c66d4609f51a87f3695aac1394cd
Pattu Raja <146655306+patturaja501@users.noreply.github.com>
2025-06-10 15:45:55 +0530

    Add files via upload

---
+++ Sprint 2/task 1/task 2/jstask2.js
@@ -0,0 +1,132 @@
+// task 1
+let userName="PattuRaja";
+console.log(userName);
+let user_name="Pattu Raja";
+console.log(user_name);
+// task 2
+const number=33.3;
+number=33.3
+console.log(number);
+// task 3
+var number=["one","two","three"];
+number.pop();
+console.log(number);
+number.push("three");
+console.log(number);
+number.shift();
+console.log(number);
+number.sort()
+console.log(number);
+// task 4
+let a=10;
+let b=20;
+let c=a+b;
+console.log(c)
+// task 5
+const PI=3.14;
+let a=5;
+let area=PI*a*a;
+console.log("area of a circle;"+area);
+// task 6
+const l=20;
+const w=10;
+let r=l*w;
+console.log(r)
+// task 7
+const b=8;
+const h=5;
+let area=(b*h)/2;
+console.log(area);
+// task 8
+let a=10;
+let b=20;
+console.log("Addition:",a+b);
+console.log("Subraction:",a-b);
+console.log("multiplication:",a*b);
+console.log("division:",a%b);
+// task 9
+let age=10;
+age +=age;
+console.log(age)
+age -=age;
+console.log(age)
+age /=age;
+console.log(age)
+// task 10
+let age=10;
+age=age +1;
+console.log(age)
+age=age -1;
+console.log(age)
+// task 11
+a=10;
+b=11;
+console.log(a==b)
+console.log(a===b)
+console.log(a>b)
+console.log(a<b)
+console.log(a!==b)
+console.log(a<=b)
+console.log(a>=b)
+// task 12
+let age=18;
+let id=false
+if(age>=18 & id)
+    console.log("allowed")
+else
+   console.log("not allowd")
+
+let atm=false;
+let gpay=true;
+if(atm||gpay)
+    console.log("allowed")
+else
+    console.log("not allowed")
+let login=true;
+if(!login)
+    console.log("login first")
+else
+    console.log("login")
+// task 13
+let a=10;
+let b=5;
+console.log("before swap:a=",a,"b=",b)
+let temp=a;
+ a=b;
+ b=temp;
+console.log("afer swap:a=",a,"b=",b)
+without using in swap
+let a=10;
+let b=5;
+console.log("before swap:a=",a,"b=",b);
+[a,b] = [b,a];
+console.log("afer swap:a=",a,"b=",b);
+
+// task 15
+let a=10;
+let b=30;
+let c=12;
+let d=3;
+let e=a+b;
+console.log("e=",e)
+let f=c*e;
+console.log("f=",f)
+let g=d%f;
+console.log("g=",g)
+// task 14
+let sub1=70;
+let sub2=70;
+let sub3=90;
+let sum=sub1+sub2+sub3;
+let average=sum/3;
+console.log("average:",average);
+// task 16
+let tamil=99;
+let english=80;
+let maths=80;
+let science=90;
+let social=85;
+let totalmark=tamil+english+maths+science+social;
+let average= totalmark/5;
+console.log("Total Mark:",totalmark)
+console.log("average:",average)
