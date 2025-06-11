e0423296e554cce6e41e5a664b2f108dd99fff43
Pattu Raja <146655306+patturaja501@users.noreply.github.com>
2025-06-11 21:11:02 +0530

    Add files via upload

---
+++ Sprint 2/task 1/task 3/jstask3.js
@@ -0,0 +1,44 @@
+// //  task 1
+//  function sayhello(){
+//     console.log("hello,world")
+
+// }
+// sayhello()
+// // task 2
+// function add(a,b){
+//     return a+b;
+// }
+// let c= add(10,10)
+// console.log(c)
+// // // task 4
+// function multiply(x,y){
+//     return x*y;
+// }
+// let c= multiply(10,10)
+// console.log(c)
+// // task 5
+// function divide(a,b){
+//     return a%b;
+// }
+// let c= divide(10,10)
+// console.log(c)
+// // task 3
+// let multiply=function(a,b){
+//     return a*b;
+// }
+// task 6
+// // console.log(multiply(10,10))
+// function factorial(n){
+//     if(n===0 || n===1){
+//         return 1;
+//     }
+
+//     return n*factorial(n-1)
+    
+// }
+// console.log(factorial(4))
+// task 7
+function square(n){
+    return n*n;
+}
+console.log(square(6))
