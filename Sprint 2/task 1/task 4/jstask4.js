847719b2b1182f785b3ee7008b17a5f1159f12c2
Pattu Raja <146655306+patturaja501@users.noreply.github.com>
2025-06-12 12:02:39 +0530

    Add files via upload

---
+++ Sprint 2/task 1/task 4/jstask4.js
@@ -0,0 +1,100 @@
+// task 1
+// let n=-1;
+// if(n>=0) {
+// console.log("this postive number");
+// }
+// else{}{
+// console.log("this is negative number");
+// }
+// task 2
+// let n=7;
+// if(n%2==0){
+//     console.log("this even number");
+// }
+// else{
+//     console.log("this odd number");
+// }
+// task 3
+// function power(base, exponent){
+//     return base**exponent;
+ 
+// }
+// console.log(power(5,2))
+// task 4
+// let a=10
+// let b=20
+// if(a>b){
+//     console.log("a is greater then b");
+// }
+// else if(b>a){
+//     console.log("b is greater then a")
+
+// }
+// else{
+//     console.log("both is equal");
+// }
+// // task 5
+// let year=2000
+// if((year %4==0 & year%100!==0)|| (year%400==0)){
+//     console.log("this  is  Leap Year");
+// }
+// else{
+//     console.log("this not a leap year");
+// }
+// // // task 6
+// let mark=
+// if(mark<=100 &(mark>=90)){
+//     console.log("Grade A")
+// }
+// else if(mark<=89 & (mark>=80)){
+//     console.log("Grade B")
+// }
+// else if(mark<=79 & (mark>=70)){
+//     console.log("Grade C")
+// }
+// else if(mark<=69 && ( mark>=60)){
+//     console.log("Grade D")
+// }
+// else if(mark>100 | mark<0){
+//     console.log("not a valid mark")
+// }
+// else {
+//     console.log("Greade f")
+// }
+// // task 7
+// let age=50
+// if(age>0 & age<16){
+//     console.log("You can't drive")
+// }
+// else if(age<=24 &age>=18){
+//     console.log("You can vote but not rent a car")
+// }
+// else if(age>=16 & age<=17){
+//     console.log("You can drive but not vote")
+// }
+// else {
+//     console.log("You can do pretty much anything")
+// }
+    // 
+//     // task 8
+// let a=15
+// if(a%15==0){
+//     console.log("Fizz Buzz")
+// }
+// else if(a%5==0){
+//     console.log("Buzz")
+// }
+// else if(a%3==0){
+//     console.log("Fizz")
+// }
+// else {
+//     console.log("number is invaild")
+// }
+// task 9
+let year=2001
+if((year %4==0 & year%100!==0)|| (year%400==0)){
+    console.log("this  is  Leap Year");
+}
+else{
+    console.log("this not a leap year");
+}
